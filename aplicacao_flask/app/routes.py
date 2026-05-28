d4a96826e55dc4b3a01e97e1f5f099e3af137ac1
Leo Silva <leo.moreira@me.com>
2026-05-28 10:58:04 -0300

    criacao de templates

--- aplicacao_flask/app/routes.py
+++ aplicacao_flask/app/routes.py
@@ -1,8 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "Página Inicial"
+    usuario = {
+        "nome": "Leo",
+        "produtos": ["Banana", "Abacaxi", "Melancia"]
+    }
+    esta_logado = True
+    return render_template("index.html", 
+                           pessoa = usuario, 
+                           usuario_logado = esta_logado)
 
 @app.route("/sobre")
 def sobre():
