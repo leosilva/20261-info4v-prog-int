c9eb59e7979765b81f51de471475da612997b4ff
Leo Silva <leo.moreira@me.com>
2026-06-11 17:59:02 -0300

    adicionando arquivo controlador de login

--- aplicacao_flask/app/routes.py
+++ aplicacao_flask/app/routes.py
@@ -1,5 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash
+from app.forms.login_form import LoginForm
+from app.controllers.AuthenticationControllers import AuthenticationController
+
 
 @app.route("/")
 def home():
@@ -19,3 +22,16 @@ def sobre():
 @app.route("/index2")
 def index2():
     return render_template('index2.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    formulario = LoginForm()
+    if formulario.validate_on_submit():
+        if AuthenticationController.login(formulario):
+             flash("Login efetuado com sucesso!")
+             return redirect('/')
+        else:
+            flash("Erro nas credenciais.")
+            return redirect('/login')
+    return render_template('login.html', title='Login', form=formulario)
